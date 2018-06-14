66ac7098c9298d8102ecd2845d824837c6538ef5
MaDengler <denglermarkus.md@gmail.com>
2018-06-14 23:56:10 +0200

    Kegelförmiger Einfall von Lichtstrahlen

--- Paraboloid.C
+++ Paraboloid.C
@@ -24,18 +24,25 @@ const double nm = AOpticsManager::nm();
 
 
 	TGeoParaboloid* para = new TGeoParaboloid("Paraboloid", 0, 120*cm,30*cm);	
-	
 	ALens* paraboloid = new ALens("ParLens", para);
 	paraboloid->SetRefractiveIndex(AGlassCatalog::GetRefractiveIndex("N-BK7"));  
-	
-	
-   	world->AddNode(paraboloid,1,new TGeoRotation("Pararot", 0,180,0));
+	world->AddNode(paraboloid,1,new TGeoRotation("Pararot", 0,180,0));
+
+
+	TGeoTube* focus = new TGeoTube("foc", 0*cm, 100*cm, 0.01*cm);
+	TGeoTranslation* foctrans = new TGeoTranslation("foctrans", 0, 0,70);
+	AFocalSurface* focal = new AFocalSurface("focal", focus);
+	world->AddNode(focal,1,foctrans);
+
+	TGeoTranslation* obstrans = new TGeoTranslation("obstrans", 0, 0,70.01);
+	AObscuration* obs = new AObscuration("Obs", focus);
+	world->AddNode(obs,1,obstrans);
+   
 	world->Draw("ogl");
 
     
-   	TGeoTranslation* phtrans = new TGeoTranslation("PhotonTrans", 0, 0,-70);
-
-    	ARayArray* Rays = ARayShooter::Circle(800, 60*cm, 3, 10, 0, phtrans, 0);
+   	TGeoTranslation* phtrans = new TGeoTranslation("PhotonTrans", 0, 0,-200);
+    	ARayArray* Rays = ARayShooter::RandomSphericalCone(440, 100,30,0, phtrans);
 	manager->TraceNonSequential(Rays);	
     
 	TObjArray* Focused = Rays -> GetFocused();
@@ -46,14 +53,20 @@ const double nm = AOpticsManager::nm();
 	std::cout << "Exited: \t" << (Exited -> GetLast())+1  << std::endl;
 	std::cout << "Suspended: \t" << (Suspended -> GetLast()) +1 << std::endl;
 
-	for(int n=0;n<(Exited -> GetLast()); n++) 
+	for(int n=0;n<(Focused->GetLast()); n++) 
 	{   
-		ARay* RayN = (ARay*)(*Exited)[n];
+		ARay* RayN = (ARay*)(*Focused)[n];
 		TPolyLine3D* polN = RayN -> MakePolyLine3D();
 		polN -> SetLineColor(2);
 		polN -> Draw();
 	}
-    
+    	for(int n=0;n<(Exited->GetLast()); n++) 
+	{   
+		ARay* RayN = (ARay*)(*Exited)[n];
+		TPolyLine3D* polN = RayN -> MakePolyLine3D();
+		polN -> SetLineColor(3);
+		polN -> Draw();
+	}
   
 
 }
